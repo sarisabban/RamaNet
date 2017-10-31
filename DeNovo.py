e9d05e538f0e8f77b415ce34148ac32707afcb27
acresearch <sari.sabban@gmail.com>
2017-10-31 20:24:28 +0300

    Updated the Database() function to accomodate for a new data set format for Machine Learning

--- DeNovo.py
+++ DeNovo.py
@@ -270,7 +270,6 @@ def Database(smaller , bigger):
 	#Collect Structures
 	os.system('wget -rA .ent.gz ftp://ftp.rcsb.org/pub/pdb/data/structures/divided/pdb/ -P DATABASE')
 	current = os.getcwd()
-	os.mkdir('Data')
 	os.mkdir('PDBDatabase')
 	filelist = os.listdir('DATABASE/ftp.rcsb.org/pub/pdb/data/structures/divided/pdb')
 	for directories in filelist:
@@ -299,11 +298,12 @@ def Database(smaller , bigger):
 		os.remove(TheFile)
 	#Remove Unwanted Structures
 	pdbfilelist = os.listdir('PDBDatabase')
+	count = 0
 	for thefile in pdbfilelist:
 		TheFile = current + '/PDBDatabase/' + thefile
 		structure = Bio.PDB.PDBParser(QUIET=True).get_structure('X' , TheFile)
 		ppb = Bio.PDB.Polypeptide.PPBuilder()
-		Type = ppb.build_peptides(structure)
+		Type = ppb.build_peptides(structure , aa_only=False)
 		#Delete Non-Protein Files
 		if Type == []:
 			print('[-] NOT PROTEIN\t' , thefile)
@@ -315,29 +315,34 @@ def Database(smaller , bigger):
 				print('[-] WRONG SIZE\t' , thefile)
 				os.remove(TheFile)
 			elif:
-				#Delete floppy structures, with loops as their dominant secondary structure
-				parser = Bio.PDB.PDBParser()
-				structure = parser.get_structure('X' , TheFile)
-				model = structure[0]
-				dssp = Bio.PDB.DSSP(model , TheFile , acc_array='Wilke')
-				SS = list()
-				for res in dssp:
-					ss = res[2]
-					if ss == '-' or ss == 'T' or ss == 'S':		#Loop (DSSP code is - or T or S)
-						SS.append('L')
-					elif ss == 'G' or ss == 'H' or ss == 'I':	#Helix (DSSP code is G or H or I)
-						SS.append('H')
-					elif ss == 'B' or ss == 'E':			#Sheet (DSSP code is B or E)
-						SS.append('S')
-				loop = SS.count('L')
-				helix = SS.count('H')
-				strand = SS.count('S')
-				if loop => helix + strand:
+				#Delete Structures With None-continuous Chains By Tracing The Chain And Measuring All The Peptide Bonds (aprox = 1.3 angstroms), If The Distance Between The C and N Atoms is Larger Than 1.3 Then There Is A Chain Break
+				structure = Bio.PDB.PDBParser(QUIET=True).get_structure('X' , TheFile)
+				ppb = Bio.PDB.Polypeptide.PPBuilder()
+				Type = ppb.build_peptides(structure , aa_only=False)
+				model = Type
+				chain = model[0]
+				count = 0
+				ChainBreak = None
+				for bond in range(length):
+					try:
+						residue1 = chain[count]
+						residue2 = chain[count + 1]
+						atom1 = residue1['C']
+						atom2 = residue2['N']
+						distance = atom1-atom2
+						count += 1
+						if distance > 1.4:
+							ChainBreak = 'Break'
+						else:
+							pass
+					except:
+						pass
+				if ChainBreak == 'Break':
 					os.remove(TheFile)
 				else:
-					continue
+					pass
 			else:
-				#Get secondary structures
+				#Get Secondary Structures
 				parser = Bio.PDB.PDBParser()
 				structure = parser.get_structure('X' , TheFile)
 				model = structure[0]
@@ -351,14 +356,32 @@ def Database(smaller , bigger):
 						SS.append('H')
 					elif ss == 'B' or ss == 'E':			#Sheet (DSSP code is B or E)
 						SS.append('S')
-				#Delete floppy structures, with loops as their dominant secondary structure
+				#Delete Floppy Structures, With Loops as Their Dominant Secondary Structure
 				loop = SS.count('L')
 				helix = SS.count('H')
 				strand = SS.count('S')
 				if loop >= helix + strand:
 					os.remove(TheFile)
 				else:
-					#Get torsion angles
+					#Renumber Residues
+					pdb = open(TheFile , 'r')
+					PDB = open('X' + TheFile , 'w')
+					count = 0
+					num = 0
+					AA2 = None
+					for line in pdb:
+						count += 1					#Sequencially number atoms
+						AA1 = line[23:27]				#Sequencially number residues
+						if not AA1 == AA2:
+							num += 1			
+						final_line = line[:7] + '{:4d}'.format(count) + line[11:17] + line[17:21] + 'A' + '{:4d}'.format(num) + line[26:]	#Update each line of the motif to have its atoms and residues sequencially labeled, as well as being in chain A
+						AA2 = AA1
+						PDB.write(final_line)				#Write to new file called motif.pdb
+					PDB.close()
+					os.remove(TheFile)
+					os.rename('X' + TheFile , TheFile)
+					#Get Torsion Angles
+					count += 1
 					Tor = list()
 					for model in Bio.PDB.PDBParser().get_structure('X' , TheFile):
 						for chain in model:
@@ -366,7 +389,7 @@ def Database(smaller , bigger):
 							for poly_index , poly in enumerate(polypeptides):
 								phi_psi = poly.get_phi_psi_list()
 								for res_index , residue in enumerate(poly):
-									#Phi angles
+									#Phi Angles
 									if phi_psi[res_index][0] is None:
 										phi = 0
 									else:
@@ -376,7 +399,7 @@ def Database(smaller , bigger):
 										while angle < -180:
 											angle = angle + 360
 										phi = angle
-									#Psi angles
+									#Psi Angles
 									if phi_psi[res_index][1] is None:
 										psi = 0
 									else:
@@ -385,20 +408,54 @@ def Database(smaller , bigger):
 											angle = angle - 360
 										while angle < -180:
 											angle = angle + 360
-										psi = angle
 									Tor.append((phi , psi))
-				#Put together
+									#Distances
+									structure = Bio.PDB.PDBParser(QUIET=True).get_structure('X' , TheFile)
+									ppb = Bio.PDB.Polypeptide.PPBuilder()
+									Type = ppb.build_peptides(structure , aa_only=False)
+									model = Type
+									chain = model[0]
+									distances = list()
+									for key , value in {0:1 , 9:11 , 19:21 , 29:31 , 39:41 , 49:51 , 59:61 , 69:71}.items(): #79:81 , 89:91
+										residue1 = chain[key]
+										residue2 = chain[length - value]
+										atom1 = residue1['CA']
+										atom2 = residue2['CA']
+										distance = atom1-atom2
+										distances.append(distance)
+				#Put Together
 				name = thefile.split('.')
-				thefile = open(name[0] + '.csv' , 'w')
-				thefile.write('residue number;secondary structure;PHI;PSI\n')
-				count = 0
-				for item in zip(SS , Tor):
-					count += 1
-					line = (str(count) + ';' + str(item[0]) + ';' + str(item[1][0]) + ';' + str(item[1][1]) + '\n')
-					thefile.write(line)
+				thefile = open('data' + '.csv' , 'a')
+				thefile.write(';1;2;3;4;5;6;7;8;9;10;11;12;13;14;15;16;17;18;19;20;21;22;23;24;25;26;27;28;29;30;31;32;33;34;35;36;37;38;39;40;41;42;43;44;45;46;47;48;49;50;51;52;53;54;55;56;57;58;59;60;61;62;63;64;65;66;67;68;69;70;71;72;73;74;75;76;77;78;79;80;81;82;83;84;85;86;87;88;89;90;91;92;93;94;95;96;97;98;99;100;101;102;103;104;105;106;107;108;109;110;111;112;113;114;115;116;117;118;119;120;121;122;123;124;125;126;127;128;129;130;131;132;133;134;135;136;137;138;139;140;141;142;143;144;145;146;147;148;149;150;Distance_1;Distance_2;Distance_3;Distance_4;Distance_5;Distance_6;Distance_7;Distance_8;Distance_9;Distance_10;\n')
+				#thefile.write(';1;2;3;4;5;6;7;8;9;10;11;12;13;14;15;16;17;18;19;20;21;22;23;24;25;26;27;28;29;30;31;32;33;34;35;36;37;38;39;40;41;42;43;44;45;46;47;48;49;50;51;52;53;54;55;56;57;58;59;60;61;62;63;64;65;66;67;68;69;70;71;72;73;74;75;76;77;78;79;80;81;82;83;84;85;86;87;88;89;90;91;92;93;94;95;96;97;98;99;100;101;102;103;104;105;106;107;108;109;110;111;112;113;114;115;116;117;118;119;120;121;122;123;124;125;126;127;128;129;130;131;132;133;134;135;136;137;138;139;140;141;142;143;144;145;146;147;148;149;150;PHI_1;PHI_2;PHI_3;PHI_4;PHI_5;PHI_6;PHI_7;PHI_8;PHI_9;PHI_10;PHI_11;PHI_12;PHI_13;PHI_14;PHI_15;PHI_16;PHI_17;PHI_18;PHI_19;PHI_20;PHI_21;PHI_22;PHI_23;PHI_24;PHI_25;PHI_26;PHI_27;PHI_28;PHI_29;PHI_30;PHI_31;PHI_32;PHI_33;PHI_34;PHI_35;PHI_36;PHI_37;PHI_38;PHI_39;PHI_40;PHI_41;PHI_42;PHI_43;PHI_44;PHI_45;PHI_46;PHI_47;PHI_48;PHI_49;PHI_50;PHI_51;PHI_52;PHI_53;PHI_54;PHI_55;PHI_56;PHI_57;PHI_58;PHI_59;PHI_60;PHI_61;PHI_62;PHI_63;PHI_64;PHI_65;PHI_66;PHI_67;PHI_68;PHI_69;PHI_70;PHI_71;PHI_72;PHI_73;PHI_74;PHI_75;PHI_76;PHI_77;PHI_78;PHI_79;PHI_80;PHI_81;PHI_82;PHI_83;PHI_84;PHI_85;PHI_86;PHI_87;PHI_88;PHI_89;PHI_90;PHI_91;PHI_92;PHI_93;PHI_94;PHI_95;PHI_96;PHI_97;PHI_98;PHI_99;PHI_100;PHI_101;PHI_102;PHI_103;PHI_104;PHI_105;PHI_106;PHI_107;PHI_108;PHI_109;PHI_110;PHI_111;PHI_112;PHI_113;PHI_114;PHI_115;PHI_116;PHI_117;PHI_118;PHI_119;PHI_120;PHI_121;PHI_122;PHI_123;PHI_124;PHI_125;PHI_126;PHI_127;PHI_128;PHI_129;PHI_130;PHI_131;PHI_132;PHI_133;PHI_134;PHI_135;PHI_136;PHI_137;PHI_138;PHI_139;PHI_140;PHI_141;PHI_142;PHI_143;PHI_144;PHI_145;PHI_146;PHI_147;PHI_148;PHI_149;PHI_150;PHI_1;PSI_2;PSI_3;PSI_4;PSI_5;PSI_6;PSI_7;PSI_8;PSI_9;PSI_10;PSI_11;PSI_12;PSI_13;PSI_14;PSI_15;PSI_16;PSI_17;PSI_18;PSI_19;PSI_20;PSI_21;PSI_22;PSI_23;PSI_24;PSI_25;PSI_26;PSI_27;PSI_28;PSI_29;PSI_30;PSI_31;PSI_32;PSI_33;PSI_34;PSI_35;PSI_36;PSI_37;PSI_38;PSI_39;PSI_40;PSI_41;PSI_42;PSI_43;PSI_44;PSI_45;PSI_46;PSI_47;PSI_48;PSI_49;PSI_50;PSI_51;PSI_52;PSI_53;PSI_54;PSI_55;PSI_56;PSI_57;PSI_58;PSI_59;PSI_60;PSI_61;PSI_62;PSI_63;PSI_64;PSI_65;PSI_66;PSI_67;PSI_68;PSI_69;PSI_70;PSI_71;PSI_72;PSI_73;PSI_74;PSI_75;PSI_76;PSI_77;PSI_78;PSI_79;PSI_80;PSI_81;PSI_82;PSI_83;PSI_84;PSI_85;PSI_86;PSI_87;PSI_88;PSI_89;PSI_90;PSI_91;PSI_92;PSI_93;PSI_94;PSI_95;PSI_96;PSI_97;PSI_98;PSI_99;PSI_100;PSI_101;PSI_102;PSI_103;PSI_104;PSI_105;PSI_106;PSI_107;PSI_108;PSI_109;PSI_110;PSI_111;PSI_112;PSI_113;PSI_114;PSI_115;PSI_116;PSI_117;PSI_118;PSI_119;PSI_120;PSI_121;PSI_122;PSI_123;PSI_124;PSI_125;PSI_126;PSI_127;PSI_128;PSI_129;PSI_130;PSI_131;PSI_132;PSI_133;PSI_134;PSI_135;PSI_136;PSI_137;PSI_138;PSI_139;PSI_140;PSI_141;PSI_142;PSI_143;PSI_144;PSI_145;PSI_146;PSI_147;PSI_148;PSI_149;PSI_150;Distance_1;Distance_2;Distance_3;Distance_4;Distance_5;Distance_6;Distance_7;Distance_8;Distance_9;Distance_10;\n')
+				ss = list()
+				for val in SS:
+					if val == 'L':
+						ss.append('1')
+					elif val == 'H':
+						ss.append('2')
+					elif val == 'S':
+						ss.append('3')
+				SecondaryStructures = ';' + ';'.join(ss)			#Secondary Structures L = 1, H = 2, S = 3 printed horisantally
+				phiang = list()
+				psiang = list()
+				for val in Tor:
+					phiang.append(val[0])
+					psiang.append(val[1])
+				PHIAngles = ';' + ';'.join(map(str, phiang))			#PHI angles printed horisantally
+				PSIAngles = ';' + ';'.join(map(str, psiang))			#PSI angles printed horisantally
+				Distances = ';' + ';'.join(map(str , distances))
+				#Fill in Remaining Positions With 0 Until Position 150
+				add = 150 - len(SS)
+				fill = list()
+				for zeros in range(add):
+					fill.append('0')
+				filling = ';' + ';'.join(fill)
+				line = str(count) + SecondaryStructures + filling + Distances	#The PHI and PSI angels are not being used because we cannot insert the angels as a feature during Machine Learning prediction, to use add this to the line variable: PHIAngles + filling + PSIAngles + filling
+				thefile.write(line)
 				thefile.close()
-				os.system('mv ' + name[0] + '.csv Data')
 				print('[+] GOOD\t' , name[0])
+	os.system('mv ' + name[0] + '.csv .')
 	os.remove('PDBDatabase')
 
 def Draw(filename):
