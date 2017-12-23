3bd756cf1cb72113b3c9df1f6f99f913d4fdc746
acresearch <sari.sabban@gmail.com>
2017-12-23 22:56:29 +0300

    Added another dataset function, the datasetCA() function

--- Database.py
+++ Database.py
@@ -276,14 +276,14 @@ def Rg(directory , RGcutoff):
 			continue
 	os.chdir(current)
 
-def Dataset(directory):
+def DatasetR(directory):
 	''' Get the secondary structures and distances '''
 	''' Generates a the data.csv with each amino acid's secondary strucure and 10 distances between the first amino acid's CA atom and others for each protein in a directory '''
 	current = os.getcwd()
 	pdbfilelist = os.listdir(directory)
 	os.chdir(directory)
 	print('\x1b[32m' + 'Getting the secondary structures of each protein' + '\x1b[0m')
-	data = open('data.csv' , 'a')
+	data = open('dataR.csv' , 'a')
 	data.write(';PDB_ID;1;2;3;4;5;6;7;8;9;10;11;12;13;14;15;16;17;18;19;20;21;22;23;24;25;26;27;28;29;30;31;32;33;34;35;36;37;38;39;40;41;42;43;44;45;46;47;48;49;50;51;52;53;54;55;56;57;58;59;60;61;62;63;64;65;66;67;68;69;70;71;72;73;74;75;76;77;78;79;80;81;82;83;84;85;86;87;88;89;90;91;92;93;94;95;96;97;98;99;100;101;102;103;104;105;106;107;108;109;110;111;112;113;114;115;116;117;118;119;120;121;122;123;124;125;126;127;128;129;130;131;132;133;134;135;136;137;138;139;140;141;142;143;144;145;146;147;148;149;150;Distance_1;Distance_2;Distance_3;Distance_4;Distance_5;Distance_6;Distance_7;Distance_8;Distance_9;Distance_10\n')
 	data.close()
 	count = 1
@@ -327,14 +327,54 @@ def Dataset(directory):
 			elif len(distances) != 10:
 				continue
 			DIline = ';'.join(distances)
-			data = open('data.csv' , 'a')
+			data = open('dataR.csv' , 'a')
 			data.write(str(count) + ';' + TheFile.split('.')[0] + ';' + SSline + ';' + DIline + '\n')
 			data.close()
 			count += 1
 		except:
 			continue
 	os.chdir(current)
-	os.rename(directory + '/data.csv' , 'data.csv')
+	os.rename(directory + '/dataR.csv' , 'dataR.csv')
+
+def DatasetCA(directory):
+	''' Get the each residue's CA atom's XYZ coordinates '''
+	''' Generates a the data.csv with the XYZ coordinates of the CA atom for each amino acid '''
+	current = os.getcwd()
+	pdbfilelist = os.listdir(directory)
+	os.chdir(directory)
+	print('\x1b[32m' + "Getting the CA atom's XYZ coordinates" + '\x1b[0m')
+	data = open('dataCA.csv' , 'a')
+	data.write(';PDB_ID;X_1;Y_1;Z_1;X_2;Y_2;Z_2;X_3;Y_3;Z_3;X_4;Y_4;Z_4;X_5;Y_5;Z_5;X_6;Y_6;Z_6;X_7;Y_7;Z_7;X_8;Y_8;Z_8;X_9;Y_9;Z_9;X_10;Y_10;Z_10;X_11;Y_11;Z_11;X_12;Y_12;Z_12;X_13;Y_13;Z_13;X_14;Y_14;Z_14;X_15;Y_15;Z_15;X_16;Y_16;Z_16;X_17;Y_17;Z_17;X_18;Y_18;Z_18;X_19;Y_19;Z_19;X_20;Y_20;Z_20;X_21;Y_21;Z_21;X_22;Y_22;Z_22;X_23;Y_23;Z_23;X_24;Y_24;Z_24;X_25;Y_25;Z_25;X_26;Y_26;Z_26;X_27;Y_27;Z_27;X_28;Y_28;Z_28;X_29;Y_29;Z_29;X_30;Y_30;Z_30;X_31;Y_31;Z_31;X_32;Y_32;Z_32;X_33;Y_33;Z_33;X_34;Y_34;Z_34;X_35;Y_35;Z_35;X_36;Y_36;Z_36;X_37;Y_37;Z_37;X_38;Y_38;Z_38;X_39;Y_39;Z_39;X_40;Y_40;Z_40;X_41;Y_41;Z_41;X_42;Y_42;Z_42;X_43;Y_43;Z_43;X_44;Y_44;Z_44;X_45;Y_45;Z_45;X_46;Y_46;Z_46;X_47;Y_47;Z_47;X_48;Y_48;Z_48;X_49;Y_49;Z_49;X_50;Y_50;Z_50;X_51;Y_51;Z_51;X_52;Y_52;Z_52;X_53;Y_53;Z_53;X_54;Y_54;Z_54;X_55;Y_55;Z_55;X_56;Y_56;Z_56;X_57;Y_57;Z_57;X_58;Y_58;Z_58;X_59;Y_59;Z_59;X_60;Y_60;Z_60;X_61;Y_61;Z_61;X_62;Y_62;Z_62;X_63;Y_63;Z_63;X_64;Y_64;Z_64;X_65;Y_65;Z_65;X_66;Y_66;Z_66;X_67;Y_67;Z_67;X_68;Y_68;Z_68;X_69;Y_69;Z_69;X_70;Y_70;Z_70;X_71;Y_71;Z_71;X_72;Y_72;Z_72;X_73;Y_73;Z_73;X_74;Y_74;Z_74;X_75;Y_75;Z_75;X_76;Y_76;Z_76;X_77;Y_77;Z_77;X_78;Y_78;Z_78;X_79;Y_79;Z_79;X_80;Y_80;Z_80;X_81;Y_81;Z_81;X_82;Y_82;Z_82;X_83;Y_83;Z_83;X_84;Y_84;Z_84;X_85;Y_85;Z_85;X_86;Y_86;Z_86;X_87;Y_87;Z_87;X_88;Y_88;Z_88;X_89;Y_89;Z_89;X_90;Y_90;Z_90;X_91;Y_91;Z_91;X_92;Y_92;Z_92;X_93;Y_93;Z_93;X_94;Y_94;Z_94;X_95;Y_95;Z_95;X_96;Y_96;Z_96;X_97;Y_97;Z_97;X_98;Y_98;Z_98;X_99;Y_99;Z_99;X_100;Y_100;Z_100;X_101;Y_101;Z_101;X_102;Y_102;Z_102;X_103;Y_103;Z_103;X_104;Y_104;Z_104;X_105;Y_105;Z_105;X_106;Y_106;Z_106;X_107;Y_107;Z_107;X_108;Y_108;Z_108;X_109;Y_109;Z_109;X_110;Y_110;Z_110;X_111;Y_111;Z_111;X_112;Y_112;Z_112;X_113;Y_113;Z_113;X_114;Y_114;Z_114;X_115;Y_115;Z_115;X_116;Y_116;Z_116;X_117;Y_117;Z_117;X_118;Y_118;Z_118;X_119;Y_119;Z_119;X_120;Y_120;Z_120;X_121;Y_121;Z_121;X_122;Y_122;Z_122;X_123;Y_123;Z_123;X_124;Y_124;Z_124;X_125;Y_125;Z_125;X_126;Y_126;Z_126;X_127;Y_127;Z_127;X_128;Y_128;Z_128;X_129;Y_129;Z_129;X_130;Y_130;Z_130;X_131;Y_131;Z_131;X_132;Y_132;Z_132;X_133;Y_133;Z_133;X_134;Y_134;Z_134;X_135;Y_135;Z_135;X_136;Y_136;Z_136;X_137;Y_137;Z_137;X_138;Y_138;Z_138;X_139;Y_139;Z_139;X_140;Y_140;Z_140;X_141;Y_141;Z_141;X_142;Y_142;Z_142;X_143;Y_143;Z_143;X_144;Y_144;Z_144;X_145;Y_145;Z_145;X_146;Y_146;Z_146;X_147;Y_147;Z_147;X_148;Y_148;Z_148;X_149;Y_149;Z_149;X_150;Y_150;Z_150\n')
+	data.close()
+	count = 1
+	for TheFile in tqdm.tqdm(pdbfilelist):
+		data = open(TheFile , 'r')
+		seen = set()
+		coordinates = list()
+		for line in data:
+			if line.split()[0] == 'ATOM' and line.split()[2] == 'CA':
+				line_lower = line.split()[5].lower()
+				if line_lower not in seen:
+					seen.add(line_lower)
+					line = [char for char in line]
+					x = ''.join(line[30:38]).strip()
+					y = ''.join(line[38:46]).strip()
+					z = ''.join(line[46:54]).strip()
+					coordinates.append(x)
+					coordinates.append(y)
+					coordinates.append(z)
+		if len(coordinates) > 450:
+			continue
+		addition = 450 - len(coordinates)
+		for zeros in range(addition):
+			coordinates.append('0')
+		COline = ';'.join(coordinates)
+		data = open('dataCA.csv' , 'a')
+		data.write(str(count) + ';' + TheFile.split('.')[0] + ';' + COline + '\n')
+		data.close()
+		count += 1
+	os.chdir(current)
+	os.rename(directory + '/dataCA.csv' , 'dataCA.csv')
 #---------------------------------------------------------------------------------------------------------------------------------------
 #Protocol to isolate specific types of structures
 Database('DATABASE' , 'PDBDatabase')	# 1. Download the PDB database
@@ -349,4 +389,5 @@ Rg('PDBDatabase' , 15)			# 9. Remove structures that are below a specified Radui
 Sequence('PDBDatabase' , 75)		# 10. Align the sequences of each structure to each structure, remove structures with similar sequences that fall above a user defined percentage
 
 #Protocol to extract specific information from isolated structures
-Dataset('PDBDatabase')			# 11. Get the secondary structures
+DatasetR('PDBDatabase')			# 11. Get the secondary structures and distances
+DatasetCA('PDBDatabase')		# 12. Get the each residue's CA atom's XYZ coordinates
