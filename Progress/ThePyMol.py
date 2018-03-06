11c50b3a1620cde71e36915a5b7e30f8e213cfd2
acresearch <sari.sabban@gmail.com>
2018-03-06 22:27:19 +0300

    Majore Organisation

--- Progress/ThePyMol.py
+++
@@ -1,8 +0,0 @@
-import pymol
-cmd.wizard('mutagenesis')
-cmd.load('Backbone.pdb')
-cmd.refresh_wizard()
-for res in range(138): cmd.get_wizard().do_select('/Backbone//A/GLY`{}'.format(res)) ; cmd.get_wizard().set_mode('VAL') ; cmd.get_wizard().apply()
-cmd.set_wizard()
-cmd.save('Topology.pdb')
-
