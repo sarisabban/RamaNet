367bb676837de80214e915fb80116ee5e72320ea
acresearch <sari.sabban@gmail.com>
2018-06-12 04:30:35 +0300

    fixed the code to work well, except for the hyperparameters of the GAN()

--- Generate.py
+++ Generate.py
@@ -75,18 +75,65 @@ class RosettaDesign():
 		#B - Perform fixbb RosettaDesign
 		packtask = standard_packer_task(pose)
 		pack = pyrosetta.rosetta.protocols.minimization_packing.PackRotamersMover(scorefxn , packtask)
-		backrub = pyrosetta.rosetta.protocols.backrub.BackrubMover()
-		backrub.pivot_residues(pose)
-		GMC = pyrosetta.rosetta.protocols.monte_carlo.GenericMonteCarloMover()
-		GMC.set_mover(backrub)
-		GMC.set_scorefxn(scorefxn)
-		GMC.set_maxtrials(500)
-		GMC.set_temperature(1.0)
-		GMC.set_preapply(False)
-		GMC.set_recover_low(True)
-		mover = pyrosetta.rosetta.protocols.moves.SequenceMover()
-		mover.add_mover(pack)
-		mover.add_mover(GMC)#####<--- problem here not accepting not rejecting moves
+		Dscore_before = 0
+		Dpose_work = Pose()
+		Dpose_lowest = Pose()
+		Dscores = []
+		Dscores.append(Dscore_before)
+		for nstruct in range(design_iters):
+			Dpose_work.assign(pose)
+			pack.apply(Dpose_work)
+			Dscore_after = scorefxn(Dpose_work)
+			Dscores.append(Dscore_after)
+			if Dscore_after < Dscore_before:
+				Dscore_before = Dscore_after
+				Dpose_lowest.assign(Dpose_work)
+			else:
+				continue
+		pose.assign(Dpose_lowest)
+		DFinalScore = scorefxn(pose)
+		#C - Output Result
+		pose.dump_pdb('structure.pdb')
+		#D - Print report
+		print('==================== Result Report ====================')
+		print('Relax Scores:\n' , Rscores)
+		print('Chosen Lowest Score:' , RFinalScore , '\n')
+		print('Design Scores:\n' , Dscores)
+		print('Chosen Lowest Score:' , DFinalScore , '\n')
+		print('BLAST result, compairing the original structure to the designed structure:')
+		RosettaDesign.BLAST(self , filename , 'structure.pdb')
+
+	def flxbb(self , filename , relax_iters , design_iters):
+		'''
+		Performs the RosettaDesign protocol to change a structure's
+		amino acid sequence while allowing for a flexible backbone.
+		Generates the structure.pdb file
+		'''
+		#A - Relax original structure
+		pose = pose_from_pdb(filename)
+		chain = pose.pdb_info().chain(1)
+		scorefxn = get_fa_scorefxn()
+		relax = pyrosetta.rosetta.protocols.relax.FastRelax()
+		relax.set_scorefxn(scorefxn)
+		Rscore_before = scorefxn(pose)
+		Rpose_work = Pose()
+		Rpose_lowest = Pose()
+		Rscores = []
+		Rscores.append(Rscore_before)
+		for nstruct in range(relax_iters):
+			Rpose_work.assign(pose)
+			relax.apply(Rpose_work)
+			Rscore_after = scorefxn(Rpose_work)
+			Rscores.append(Rscore_after)
+			if Rscore_after < Rscore_before:
+				Rscore_before = Rscore_after
+				Rpose_lowest.assign(Rpose_work)
+			else:
+				continue
+		pose.assign(Rpose_lowest)
+		RFinalScore = scorefxn(pose)
+		#B - Perform flxbb RosettaDesign
+		mover = pyrosetta.rosetta.protocols.flxbb.FlxbbDesign()
 		Dscore_before = 0
 		Dpose_work = Pose()
 		Dpose_lowest = Pose()
@@ -114,13 +161,6 @@ class RosettaDesign():
 		print('Chosen Lowest Score:' , DFinalScore , '\n')
 		print('BLAST result, compairing the original structure to the designed structure:')
 		RosettaDesign.BLAST(self , filename , 'structure.pdb')
-
-	def flxbb(self , filename , relax_iters , design_iters):
-		'''
-		Performs the RosettaDesign protocol to change a structure's
-		amino acid sequence while allowing for a flexible backbone.
-		Generates the structure.pdb file
-		'''
 
 def Fragments(filename):
 	'''
@@ -220,14 +260,14 @@ def Fragments(filename):
 	rmsd.close()
 	#Analyse the RMSD file to get the lowest RMSD for each position
 	data = open('RMSDvsPosition.dat' , 'w')
-	lowest = {} 												#Mapping group number -> lowest value found
+	lowest = {} 				#Mapping group number -> lowest value found
 	for line in open('temp.dat'):
 		parts = line.split()
-		if len(parts) != 2:										#Only lines with two items on it
+		if len(parts) != 2:		#Only lines with two items on it
 			continue
 		first = float(parts[0])
 		second = int(parts[1])
-		if first == 0: 											#Skip line with 0.0 RMSD (this is an error from the 9-mer fragment file). I don't know why it happens
+		if first == 0: 			#Skip line with 0.0 RMSD (this is an error from the 9-mer fragment file). I don't know why it happens
 			continue
 		if second not in lowest:
 			lowest[second] = first
@@ -277,33 +317,23 @@ def Fragments(filename):
 	os.remove('temp.dat')
 	return(Average_RMSD)
 
-def FoldPDB_PSC(line):
+def FoldPDB_PSC(data):
 	'''
 	Fold a primary structure using the phi and psi torsion
-	angles as well as the CA atom constraints
+	angles as well as the CA atom constraints.
 	Generates the Backbone.pdb file
 	'''
-	size = int(len(line.split(';')) / 3)
+	#Generate a pose
+	size = int(len(data[0]))
 	Vs = list()
 	for numb in range(size):
 		Vs.append('V')
 	sequence = ''.join(Vs)
 	pose = pose_from_sequence(sequence)
 	#Isolate each angle and constraint
-	tick = 0
-	PHI = list()
-	PSI = list()
-	CST = list()
-	for angle in line.split(';'):
-		if tick == 0:
-			PHI.append(angle)
-			tick = 1
-		elif tick == 1:
-			PSI.append(angle)
-			tick = 2
-		else:
-			CST.append(angle)
-			tick = 0
+	PHI = data[0]
+	PSI = data[1]
+	CST = data[2]
 	count = 1
 	#Move amino acids angles
 	for P , S in zip(PHI , PSI):
@@ -319,7 +349,7 @@ def FoldPDB_PSC(line):
 		thefile.close()
 		atom += 1
 	#Add constraints option to pose
-	constraints = pyrosetta.rosetta.protocols.simple_moves.ConstraintSetMover()
+	constraints = pyrosetta.rosetta.protocols.constraint_movers.ConstraintSetMover()
 	constraints.constraint_file('constraints.cst')
 	constraints.add_constraints(True)
 	constraints.apply(pose)
@@ -431,19 +461,21 @@ def GAN():
 	phiout = gen[0::3]	#[start:end:step]
 	psiout = gen[1::3]	#[start:end:step]
 	cstout = gen[2::3]	#[start:end:step]
-	#Renormalise
+	#Re-normalise
 	phiout = [x*360.0 for x in phiout]
-	psiout = [x*360.0 for x in phiout]
-	cstout = [x*207.801 for x in phiout]
+	psiout = [x*360.0 for x in psiout]
+	cstout = [x*207.801 for x in cstout]
 	return(phiout, psiout, cstout)
 
 def main():
-	line = GAN()			##### <-------- Requires Work
-	FoldPDB_PSC(line)
+	data = GAN()
+	FoldPDB_PSC(data)
 	RD = RosettaDesign()
-	RD.fixbb('Backbone.pdb')	##### <-------- Requires Work
+	RD.flxbb('Backbone.pdb')
 	os.remove('Backbone.pdb')
 	Fragments('structure.pdb')
 
 if __name__ == '__main__':
 	main()
+
+### WORK ON THE GAN() METHOD
