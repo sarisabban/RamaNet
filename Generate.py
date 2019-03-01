39bd39eb78b2c41fe53f85975ce9782d88e4f7ba
acresearch <sari.sabban@gmail.com>
2019-03-01 19:18:07 +0300

    Adding a reference

--- Generate.py
+++ Generate.py
@@ -1424,7 +1424,27 @@ def FoldPDB_PSC(data):
 def LSTM_GAN(choice):
 	'''
 	A neural network that designs a helical protein topology using phi/psi angels
-	The neural network was chosen and optamised by Mikhail Markovsky
+	The neural network was chosen and optamised by Mikhail Markovsky.
+	-----------------------------------------------------------------------------------
+	This neural network architecture was inspired from https://github.com/nesl/sensegen 
+	that has the following licence:
+
+	All rights reserved Networked and Embedded Systems Lab (NESL), UCLA.
+	Permission is hereby granted, free of charge, to any person obtaining a copy 
+	of this software and associated documentation files (the "Software"), to deal 
+	in the Software without restriction, including without limitation the rights 
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies 
+	of the Software, and to permit persons to whom the Software is furnished to do 
+	so, subject to the following conditions:
+	The above copyright notice and this permission notice shall be included in all 
+	copies or substantial portions of the Software.
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS 
+	FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR 
+	COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER 
+	IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION 
+	WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+	-----------------------------------------------------------------------------------
 	'''
 	TRAIN_DATA_FILE = './PS_Helix_500.csv'	# Dataset location
 	NUM_EPOCHS = 3000						# Number of training epochs
